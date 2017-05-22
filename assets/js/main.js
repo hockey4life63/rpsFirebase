37f9a7bd85c3973eed07668c6bd13a13e105e360
hockey4life63 <Amelancon68@gmail.com>
2017-05-21 17:16:30 -0700

    added the start of the game logic and made gaem delete data as users leave

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
-$('.modal').modal({ backdrop: 'static', keyboard: false })
-$(".modal").modal("show")
+$('#loginBox').modal({ backdrop: 'static', keyboard: false })
+
+// $("#loginBox").modal("show")
+
 
 let database = firebase.database();
 let userRef = database.ref("/users");
@@ -12,6 +14,27 @@ let state = {
     winner: 3,
     playagian: 4,
     quit: 5
+}
+let timerObj = {
+    timerID: "",
+    perTurn: 15,
+    start: function() {
+        let timerCount = this.perTurn;
+        timeLoc = $("#timeLeft");
+        timeLoc.text(timerCount);
+        this.timerID = setInterval(function() {
+            timerCount--;
+            timeLoc.text(timerCount);
+            if (timerCount === 0) {
+                timerObj.stop();
+            }
+
+        })
+    },
+    stop: function() {
+        clearInterval(this.timerID);
+    }
+
 }
 $("#nameSubmit").on("click", function() {
     let name = $("#nameText").val().trim();
@@ -28,6 +51,7 @@ $("#nameSubmit").on("click", function() {
             currentUser = name;
             $(".modal").modal("hide")
             callGameBtn();
+            userRef.child(name).onDisconnect().remove()
         } else {
             $("#nameInfo").text("That name is taken try another")
         }
@@ -40,11 +64,12 @@ $("#createGame").on("click", function() {
         createrName: currentUser,
         joinerName: "",
         state: state.open,
+        hostChoice: "",
+        joinerChoice: ""
     }
     newRef.set(newItem).then(function() {
         gameState(newRef.key)
     })
-    $(this).addClass("disabled").off()
 })
 
 function callGameBtn() {
@@ -56,7 +81,6 @@ function callGameBtn() {
     })
     gameRef.on("child_changed", function(snap) {
         if (snap.val().state !== state.open) {
-            console.log(snap.key)
             $("#" + snap.key).remove()
 
         }
@@ -93,8 +117,65 @@ function createGameBtn(key) {
     })
 }
 
+function setGameBox(key) {
+    let name = ""
+    gameRef.child(key).once("value", function(snap) {
+        if (currentUser === snap.val().joinerName) {
+            $("#theirName").text(snap.val().createrName)
+        } else {
+            $("#theirName").text(snap.val().joinerName)
+        }
+    }).then(function() {
+        $("#gameBox").modal({ backdrop: 'static', keyboard: false });
+        $("#gameBox").off("click")
+        $(document).off("keypress")
+
+
+        $("#chatBox").html("");
+        setupChat(key);
+    })
+
+}
+
+function makeMessage(name, message) {
+    let div = $("<div>");
+    let newP = $("<p>")
+    div.addClass("message");
+    newP.text(name + ": " + message);
+    div.append(newP);
+    $("#chatBox").append(div);
+}
+
+function setupChat(key) {
+    gameRef.child(key + "/chat").on("child_added", function(snap) {
+        if (snap.val().name !== undefined) {
+            makeMessage(snap.val().name, snap.val().message)
+        }
+    })
+    $("#textSubmit").on("click", function() {
+        if ($("#textMessage").val().trim() !== "") {
+            gameRef.child(key + "/chat").push().set({
+                name: currentUser,
+                message: $("#textMessage").val().trim()
+            })
+            $("#textMessage").val("");
+        }
+    })
+}
+
+function quitGame(key) {
+    $("#gameBox").modal("hide")
+    $(".allGames").show();
+    gameRef.child(key + "/chat").off();
+    gameRef.child(key).off();
+    $("#textSubmit").off("click");
+    $("#choiceSubmit").off("click");
+    $(".rpsChoice").off("click");
+    callGameBtn();
+}
+
 function gameState(key) {
-    // 	state: {
+    //  state: {
     //     open: 0,
     //     joined: 1,
     //     choose: 2,
@@ -103,26 +184,90 @@ function gameState(key) {
     //     quit: 5
     // }
     let currentGame = gameRef.child(key);
-
-    currentGame.on("value", function(snap) {
+    let currentChoice = "";
+    let host = false;
+    currentGame.once("value", function(snap) {
+        if (snap.val().createrName === currentUser) {
+            host = true;
+        }
+    })
+    currentGame.child("state").on("value", function(snap) {
         let data = snap.val();
-        switch (data.state) {
+        if (data === null) {
+            quitGame(key);
+        }
+        switch (data) {
             case state.open:
                 gameRef.orderByChild("state").equalTo(state.open).off()
                 $(".allGames").hide()
                 console.log("game is open")
+                currentGame.child("chat").set({
+                    chat: true
+                });
+                currentGame.onDisconnect().remove()
                 break;
 
             case state.joined:
+                setGameBox(key);
+                if (!host) {
+                    currentGame.onDisconnect().remove()
+                }
+                currentGame.once("value", function(snap) {
+                    if (host) {
+                        currentGame.update({
+                            state: state.choose
+                        })
+                    }
+                })
+
 
                 console.log("game is joined");
                 break;
 
             case state.choose:
+                $(".rpsChoice").on("click", function() {
+                    currentChoice = $(this).attr("choice");
+                })
+                $("#choiceSubmit").on("click", function() {
+                    currentGame.once("value", function(snap) {
+                        if (host) {
+                            currentGame.update({
+                                hostChoice: currentChoice
+                            })
+                        } else {
+                            currentGame.update({
+                                joinerChoice: currentChoice
+                            })
+                        }
+
+                    }).then(function(snap) {
+                        if (host) {
+                            currentGame.once("value", function(snap) {
+                                if (snap.val().joinerChoice !== "") {
+                                    currentGame.update({
+                                        state: state.winner
+                                    })
+                                }
+                            })
+                        } else {
+                            currentGame.once("value", function(snap) {
+                                if (snap.val().hostChoice !== "") {
+                                    currentGame.update({
+                                        state: state.winner
+                                    })
+                                }
+                            })
+                        }
+                    })
+
+                })
 
                 break;
 
             case state.winner:
+                $("#choiceSubmit").off("click")
+                $(".rpsChoice").off("click")
+                console.log("Made it to Winner State!!!!!")
 
                 break;
 
